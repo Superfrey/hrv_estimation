f38c7dc4ba061f0d4082d12bce9fd536e4500fc8
Jonas R Schaarup <jonasfrschaarup@gmail.com>
2023-01-02 10:27:51 +0100

    Github ready for help

---
+++ R/function.R
@@ -0,0 +1,57 @@
+## load package
+
+library('RHRV')
+library(vroom)
+library(here)
+library(tidyverse)
+
+# Function for getting individual HRV estimates # works well
+
+rhrv_file <- function(hrv_id_values) {
+    hrv_id_values <- cumsum(hrv_id_values/1000)
+
+    rr_data <- RHRV::CreateHRVData() %>%
+        RHRV::LoadBeatVector(hrv_id_values) %>%
+        RHRV::BuildNIHR()  %>%
+        RHRV::FilterNIHR() %>%  #consider with an without
+        RHRV::InterpolateNIHR() %>%
+        RHRV::CreateTimeAnalysis()
+
+    hrv_estimates <- c(SDNN = rr_data$TimeAnalysis[[1]]$SDNN,
+                       RMSSD = rr_data$TimeAnalysis[[1]]$rMSSD,
+                       pNN50 = rr_data$TimeAnalysis[[1]]$pNN50,
+                       SDSD = rr_data$TimeAnalysis[[1]]$SDSD,
+                       SDANN = rr_data$TimeAnalysis[[1]]$SDANN)
+
+    return(hrv_estimates)
+}
+
+# loop function for doing hrv analysis on a merged dataset with all individuals
+# does not work
+##Error in `select()`:
+#! Can't subset columns with `as.numeric(rr_data_column)`.
+#✖ Can't convert from `as.numeric(rr_data_column)` <double> to <integer> due to loss of precision.
+#Backtrace:
+#   1. global rhrv_df(rr_df, rr_df$user_id, rr_df$ibi_s)
+# 20. rlang::cnd_signal(x)
+
+rhrv_df <- function(df_rr_long, id_rr, rr_data_column) {
+    id <- c(id_rr)
+
+    df <- data.frame(id_rr)
+
+    for (i in id) {
+        summarised_hrv <- df_rr_long %>%
+            filter(id_rr == i) %>%
+            select(as.numeric(rr_data_column))
+
+        hrv_indices<- rhrv_file(summarised_hrv)
+    }
+
+    df <- rbind(df,hrv_indices)
+
+    return(df)
+}
+
+rhrv_df(rr_df,rr_df$user_id, rr_df$ibi_s)
+
--- R/hrv_function_sandbox.R
+++ R/hrv_function_sandbox.R
@@ -25,27 +25,67 @@ mylist <- runif(n = 100, min = 600, max = 1000)
 #return(hrv_estimates)
 #    }
 
+#test dataset for hrv
+
+hrv_test <- cumsum(hrv_test/1000)
+
+my_data <- CreateHRVData() %>%
+    LoadBeatVector(hrv_test) %>%
+    BuildNIHR()  %>%
+    FilterNIHR() %>%
+    InterpolateNIHR() %>%
+    CreateTimeAnalysis()
+
+
 
 # MAKE A NUMERIC LIST OF VALUE BASED ON COLUMN WITH HR OR IBI
 # EXTRACT COLUMN AND INSERT BY ID
-hrv_file <- function(hrv_id_values) {
+
+rhrv_file <- function(hrv_id_values) {
     hrv_id_values <- cumsum(hrv_id_values/1000)
 
     rr_data <- RHRV::CreateHRVData() %>%
         RHRV::LoadBeatVector(hrv_id_values) %>%
         RHRV::BuildNIHR()  %>%
-        RHRV::FilterNIHR() %>%
+        RHRV::FilterNIHR() %>%  #consider with an without
         RHRV::InterpolateNIHR() %>%
         RHRV::CreateTimeAnalysis()
 
-    hrv_estimates <- c(SDNN = rr_data$TimeAnalysis[[1]]$SDNN, RMSSD = rr_data$TimeAnalysis[[1]]$rMSSD, )
+    hrv_estimates <- c(SDNN = rr_data$TimeAnalysis[[1]]$SDNN,
+                       RMSSD = rr_data$TimeAnalysis[[1]]$rMSSD,
+                       pNN50 = rr_data$TimeAnalysis[[1]]$pNN50,
+                       SDSD = rr_data$TimeAnalysis[[1]]$SDSD,
+                       SDANN = rr_data$TimeAnalysis[[1]]$SDANN)
 
     return(hrv_estimates)
 }
 
-hrvdatatest <- hrv_file(hrv_test)
-hrvdatatest
+rhrvdatatest <- hrv_file(hrv_test)
+rhrvdatatest
 
+# Long format
+# Long format #solution could be in for function...
+rhrv_df <- function(df_rr_long, id_rr, rr_data_column) {
+    summarised_hrv <- df_rr_long %>%
+        group_by(id_rr) %>%
+        select(rr_data_column)
+
+    rhrv_file(summarised_hrv)
+
+    return(summarised_hrv)
+}
+
+rhrv_df()
+# tester
+
+rr_data <- RHRV::CreateHRVData() %>%
+    RHRV::LoadBeatVector(hrv_test) %>%
+    RHRV::BuildNIHR()  %>%
+    RHRV::FilterNIHR() %>%
+    RHRV::InterpolateNIHR() %>%
+    RHRV::CreateTimeAnalysis()
+
+r3::check_setup()
 158.81411  64.25367
 sdnn <- hrvdatatest$TimeAnalysis[[1]]$SDNN
 sdnn
@@ -86,7 +126,7 @@ hrv_data = CalculatePowerBand(hrv_data , indexFreqAnalysis= 1,
 test$hrv[1] <- hrv_data$TimeAnalysis$SDNN
 ```
 
-
+# RHRV described workflow
 data("HRVData")
 data("HRVProcessedData")
 hrv_data = RHRV::CreateHRVData()
@@ -104,15 +144,6 @@ hrv_data$TimeAnalysis$rMSSD
 hrv_estimates <- hrv_data$TimeAnalysis$SDNN
 hrv_estimates
 
-
-hrv_test <- cumsum(hrv_test/1000)
-
-my_data <- CreateHRVData() %>%
-    LoadBeatVector(hrv_test) %>%
-    BuildNIHR()  %>%
-    FilterNIHR() %>%
-    InterpolateNIHR() %>%
-    CreateTimeAnalysis()
 
 
 
